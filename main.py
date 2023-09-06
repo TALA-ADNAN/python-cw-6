2f43c552d68a05cb82437310a5035fb9e6f40c75
TALA-ADNAN <iit367372@gmail.com>
2023-09-06 19:50:03 +0300

    CW6

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 # write your code here
+person = {
+    "name": "FARAH",
+    "age": 21,
+    "hobbies": ["Football, swimming, singing"]
+}
+print(person["name"])
+print(person["age"])
+person["age"] = 22
+print(person["age"])
+person["country"] = "UK"
+print(person)
+print(len(person))
+person["hobbies"] = "drawing"
+print(person)
+
+
+def check_hobbies(person):
+    if len(person["hobbies"]) > 3:
+        print("WOW YOU ARE AMAZ")
+    else:
+        print("Oops")
+
+
+check_hobbies(person)
